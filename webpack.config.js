6268f17d641906655ca308673beef699a786585b
Slimane GASSAMA <sgassama@bouyguestelecom.fr>
2019-12-17 19:20:23 +0100

    STARTWARS fix(integ): imag path & some modifs

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 const OptimizeCSSAssets = require("optimize-css-assets-webpack-plugin");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 let config = {
   entry: ["./src/index.js", "./assets/stylesheets/styles.scss"],
@@ -16,30 +16,31 @@ let config = {
       exclude: /node_modules/,
       loader: "babel-loader"
     },
-      {
-        test: /\.scss$/,
-        use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader', 'postcss-loader'],
-        }))
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: 'file-loader?name=[name].[ext]&outputPath=images/',
-      },
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            attrs: [':data-src']
-          }
+    {
+      test: /\.scss$/,
+      use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader', 'postcss-loader'],
+      }))
+    },
+    {
+      test: /\.(png|jpg|gif|svg)$/,
+      use: 'file-loader?name=[name].[ext]&outputPath=images/',
+    },
+    {
+      test: /\.(html)$/,
+      use: {
+        loader: 'html-loader',
+        options: {
+          attrs: [':data-src']
         }
-      }]
+      }
+    }
+  ]
   },
   plugins: [
     new ExtractTextWebpackPlugin("styles.css"),
-    new UglifyJSPlugin(),
+    new UglifyJsPlugin(),
   ],
   devServer: {
     contentBase: path.resolve(__dirname, "./public"),
