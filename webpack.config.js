8be3b71d1a2adc847587bd77cdef9bf91bd02712
Slimane GASSAMA <sgassama@bouyguestelecom.fr>
2021-02-28 19:33:37 +0100

    chores: remove some packages

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,34 @@ const allConfig = smp.wrap({
     },
     {
       test: /\.(png|jpg|gif|svg)$/,
-      use: 'file-loader?name=[name].[ext]&outputPath=images/',
+      // use: 'file-loader?name=[name].[ext]&outputPath=images/',
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/'
+          }
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+            },
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [0.65, 0.90],
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false,
+            }
+          }
+        },
+      ],
     },
   ]
   },
